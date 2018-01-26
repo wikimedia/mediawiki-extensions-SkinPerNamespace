2658b22c0aaa193d68569dcdc1e77981afec63d0
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-01-26 21:18:48 +0100

    Convert SkinPerNamespace to use extension registration
    Bug: T185762
    Change-Id: I6de293bb11d666ad3f3587ee402868493eeacf40

--- SkinPerNamespace.php
+++ SkinPerNamespace.php
@@ -1,83 +1,32 @@
 <?php
 /**
- * Extension based on SkinPerPage to allow a customized skin per namespace
+ * MediaWiki extension SkinPerNamespace
+ * Copyright (C) 2012,  Alexandre Emsenhuber
  *
- * Require MediaWiki 1.19.0 or greater.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * @file
- * @author Alexandre Emsenhuber
- * @license GPLv2
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
-$wgHooks['RequestContextCreateSkin'][] = 'efSkinPerPageRequestContextCreateSkin';
-
-// Add credits :)
-$wgExtensionCredits['other'][] = array(
-	'path'        => __FILE__,
-	'name'        => 'SkinPerNamespace',
-	'url'         => 'https://www.mediawiki.org/wiki/Extension:SkinPerNamespace',
-	'version'     => '2014-04-01',
-	'descriptionmsg' => 'skinpernamespace-desc',
-	'author'      => 'Alexandre Emsenhuber',
-);
-
-$wgMessagesDirs['SkinPerNamespace'] = __DIR__ . '/i18n';
-
-// Configuration part, you can copy it to your LocalSettings.php and change it
-// there, *not* here. Also modify it after including this file or you won't see
-// any changes.
-
-/**
- * Array mapping namespace index (i.e. numbers) to skin names
- */
-$wgSkinPerNamespace = array();
-
-/**
- * Skins for special pages, mapping canonical name (see SpecialPage.php) to skin
- * names
- */
-$wgSkinPerSpecialPage = array();
-
-/**
- * Override preferences for logged in users ?
- * if set to false, this will only apply to anonymous users
- */
-$wgSkinPerNamespaceOverrideLoggedIn = true;
-
-// Implementation
-
-/**
- * Hook function for RequestContextCreateSkin
- * @param IContextSource $context
- * @param Skin $skin
- * @return bool
- */
-function efSkinPerPageRequestContextCreateSkin( $context, &$skin ) {
-	global $wgSkinPerNamespace, $wgSkinPerSpecialPage,
-		$wgSkinPerNamespaceOverrideLoggedIn;
-
-	if ( !$wgSkinPerNamespaceOverrideLoggedIn && $context->getUser()->isLoggedIn() ) {
-		return true;
-	}
-
-	$title = $context->getTitle();
-	$ns = $title->getNamespace();
-	$skinName = null;
-
-	if ( $ns == NS_SPECIAL ) {
-		list( $canonical, /* $subpage */ ) = SpecialPageFactory::resolveAlias( $title->getDBkey() );
-		if ( isset( $wgSkinPerSpecialPage[$canonical] ) ) {
-			$skinName = $wgSkinPerSpecialPage[$canonical];
-		}
-	}
-
-	if ( $skinName === null && isset( $wgSkinPerNamespace[$ns] ) ) {
-		$skinName = $wgSkinPerNamespace[$ns];
-	}
-
-	if ( $skinName !== null ) {
-		$skin = $skinName;
-	}
-
-	return true;
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'SkinPerNamespace' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['SkinPerNamespace'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for the SkinPerNamespace extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the SkinPerNamespace extension requires MediaWiki 1.29+' );
 }
---
+++ SkinPerNamespaceHooks.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Hook function for RequestContextCreateSkin
+ * This file is part of the MediaWiki extension SkinPerNamespace
+ *
+ * Copyright (C) 2012, Alexandre Emsenhuber
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+class SkinPerNamespaceHooks {
+	/**
+	 * Hook function for RequestContextCreateSkin
+	 * @param IContextSource $context
+	 * @param Skin $skin
+	 * @return bool
+	 */
+	static function onSkinPerPageRequestContextCreateSkin( $context, &$skin ) {
+		global $wgSkinPerNamespace, $wgSkinPerSpecialPage,
+			$wgSkinPerNamespaceOverrideLoggedIn;
+
+		if ( !$wgSkinPerNamespaceOverrideLoggedIn && $context->getUser()->isLoggedIn() ) {
+			return true;
+		}
+
+		$title = $context->getTitle();
+		$ns = $title->getNamespace();
+		$skinName = null;
+
+		if ( $ns === NS_SPECIAL ) {
+			list( $canonical, /* $subpage */ ) = SpecialPageFactory::resolveAlias( $title->getDBkey() );
+			if ( isset( $wgSkinPerSpecialPage[$canonical] ) ) {
+				$skinName = $wgSkinPerSpecialPage[$canonical];
+			}
+		}
+
+		if ( $skinName === null && isset( $wgSkinPerNamespace[$ns] ) ) {
+			$skinName = $wgSkinPerNamespace[$ns];
+		}
+
+		if ( $skinName !== null ) {
+			$skin = $skinName;
+		}
+
+		return true;
+	}
+}
